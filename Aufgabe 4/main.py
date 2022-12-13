f68737e14fbdf6f5c430092b8e237420e68c28c0
Alexander Mühlegger <amuehlegger@tsn.at>
2022-12-13 22:52:04 +0100

    final aufgabe 4

--- Aufgabe 4/main.py
+++ Aufgabe 4/main.py
@@ -24,6 +24,11 @@ def getPred(df, df_pred):
     predictions = model.predict(df_pred)
     return predictions
 
+def getModel(y, v):
+    df_reg=plt.DataFrame({"years": y, "bev":v})
+    model =  sm.OLS.from_formula('bev ~ years', df_reg).fit()
+    return model
+
 predictions = getPred(df_reg, df_pred) 
 prediction = getPred(df_reg, pd.DataFrame({'years': [2030]}))
 
@@ -47,3 +52,26 @@ plt.plot(df_gem_reg.years, gem_sum_data)
 plt.plot(df_pred.years, predictions_gem)
 plt.show()
 # print(df_gem)
+
+fig, axes = plt.subplots(2,1)
+t1 = df.values[1:24, 3:].sum(axis=0)
+d1 = pd.DataFrame({"Bevölkerungszahl":t1})
+t2 = df.values[110:139, 3:].sum(axis=0)
+d2 = pd.DataFrame({"Bevölkerungszahl":t2})
+
+
+d1.plot(ax=axes[0], title="Bezirk Innsbruck")
+d2.plot(ax=axes[1], title="Bezirk Kufstein")
+
+fig.tight_layout()
+plt.show()
+
+plt.plot(df.columns[3:], d1, label="I")
+plt.plot(df.columns[3:], d2, label="KU")
+plt.legend()
+plt.show()
+
+plt.plot(df.columns[3:], getModel(pd.to_numeric(df.columns[3:]), pd.to_numeric(df.values[1:24,3:].sum(axis=0))).predict(pd.DataFrame({"years":np.arange(1993,2022)})), label="IM")
+plt.plot(df.columns[3:], getModel(pd.to_numeric(df.columns[3:]), pd.to_numeric(df.values[110:139,3:].sum(axis=0))).predict(pd.DataFrame({"years":np.arange(1993,2022)})), label="KU")
+plt.legend()
+plt.show()
